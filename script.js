fb6e6398df281c6d9d431cbe5f92b9a683de9406
adzizPe <146005480+adzizPe@users.noreply.github.com>
2024-05-05 02:34:51 +0700

    first

---
+++ script.js
@@ -0,0 +1,46 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+}
+
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('active'); 
+    navbar.classList.toggle('active'); 
+});
+
+
+$(document).ready(function () {
+    
+    $("a").on('click', function (event) {
+
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                window.location.hash = hash;
+            });
+        } 
+    });
+});
+
