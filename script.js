cbebbc6dc7b1911cd47dc28dc9710f11291050ee
adzizPe <adzanaziz2@gmail.com>
2024-06-14 00:07:13 +0700

    update

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ $(document).ready(function () {
 
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
-            }, 400, function () {
+            }, 200, function () {
 
                 window.location.hash = hash;
             });
